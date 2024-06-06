04e2d21796b20557e1196583f87772979d08a68c
Your Name <you@example.com>
2024-06-05 21:19:07 -0700

    Add keymaps directory

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,13 @@
-vim.cmd("inoremap {V {}")
-vim.keymap.set('i', 'kj', '<Esc>', { desc = "[k]wuickly [j]ump back to -- Normas mode" })
+--[[
+
+--]]
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+vim.cmd("set hidden")
+
+require("keymaps")
+
+-- vim: ts=2 sts=2 sw=2 et
